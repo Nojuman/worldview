b5de534899a8c2a8d511f94cc8fb003643db3c55
ZachTRice <zachtrice@gmail.com>
2018-02-17 22:26:57 -0500

    Adjust buster assertions to check HH:MM:SS & correct time methods - Removed util.thisTime util function in favor of util.now - Changed buster assertions to check for HH:MM:SS in addition to YYYY-MM-DD - Restored clearTimeUTC method functionality

--- web/js/util/util.js
+++ web/js/util/util.js
@@ -265,9 +265,9 @@ export default (function (self) {
    * @return {Date} the date object
    */
   self.clearTimeUTC = function (date) {
-    // date.setUTCHours(0);
-    // date.setUTCMinutes(0);
-    // date.setUTCSeconds(0);
+    date.setUTCHours(0);
+    date.setUTCMinutes(0);
+    date.setUTCSeconds(0);
     date.setUTCMilliseconds(0);
     return date;
   };
@@ -554,7 +554,7 @@ export default (function (self) {
 
   /**
    * Gets the current day. Use this instead of the Date methods to allow
-   * debugging alternate "now" times.
+   * debugging alternate "now" dates.
    *
    * @method today
    * @static
@@ -563,10 +563,6 @@ export default (function (self) {
    */
   self.today = function () {
     return self.clearTimeUTC(self.now());
-  };
-
-  self.thisTime = function () {
-    return self.now();
   };
 
   /**
